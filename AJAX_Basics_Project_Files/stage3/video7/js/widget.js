cf65e75d122fcc473867f5ec7a3797c73e547dbe
ANN HEALY <amheal01@gmail.com>
2018-05-10 20:51:54 -0400

    Update to AJAX with jQuery

--- AJAX_Basics_Project_Files/stage3/video7/js/widget.js
+++ AJAX_Basics_Project_Files/stage3/video7/js/widget.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-  $.getJSON('../data/employees.json', function (data) {
+  console.log(1);
+  $.getJSON('data/employees.json', function (data) {
     var statusHTML = '<ul class="bulleted">';
     $.each(data,function (index, employee) {
       if (employee.inoffice === true) {
@@ -11,5 +12,27 @@ $(document).ready(function () {
     });
     statusHTML += '</ul>';
     $('#employeeList').html(statusHTML)
+  }); // end getJSON
+}); // end ready
+
+
+$(document).ready(function () {
+  console.log(2);
+  $.getJSON('data/rooms.json', function (data) {
+    var roomsHTML = '<ul class="rooms">';
+    $.each(data,function (index, room) {
+      if (room.available === true) {
+        roomsHTML +='<li class="empty">';
+        console.log(3)
+      } else {
+        roomsHTML +='<li class="full">';
+        console.log(4)
+      }
+      roomsHTML += room.room + '</li>';
+      console.log(5)
+    });
+    roomsHTML += '</ul>';
+    $('#roomList').html(roomsHTML)
+    console.log(roomsHTML);
   }); // end getJSON
 }); // end ready
